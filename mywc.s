fbc91f4155db5d815172466ffdb128eaca57a6b7
Ishaan Javali <ijavali@princeton.edu>
2022-11-27 13:38:47 -0500

    flattened c mywc.s

---
+++ mywc.s
@@ -0,0 +1,51 @@
+/*--------------------------------------------------------------------*/
+/* mywc.c                                                             */
+/* Author: Bob Dondero                                                */
+/*--------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+/*--------------------------------------------------------------------*/
+
+/* In lieu of a boolean data type. */
+enum {FALSE, TRUE};
+
+/*--------------------------------------------------------------------*/
+
+static long lLineCount = 0;      /* Bad style. */
+static long lWordCount = 0;      /* Bad style. */
+static long lCharCount = 0;      /* Bad style. */
+static int iChar;                /* Bad style. */
+static int iInWord = FALSE;      /* Bad style. */
+
+/*--------------------------------------------------------------------*/
+
+/* Write to stdout counts of how many lines, words, and characters
+   are in stdin. A word is a sequence of non-whitespace characters.
+   Whitespace is defined by the isspace() function. Return 0. */
+
+int main(void)
+{
+   loop1:
+      if((iChar = getchar()) == EOF) goto endloop1;
+      lCharCount ++;
+         
+         if(! isspace(iChar)) goto else1;
+            if(!iInWord) goto endif1;
+            lWordCount++;
+            iInWord = FALSE;
+            goto endif1;
+      else1:
+         if(iInWord) goto endif1;
+         iInWord = TRUE;
+      endif1:
+         if(iChar == '\n') iLineCount++;
+         goto loop1;
+   endloop1;
+   
+   if (iInWord) lWordCount++;
+
+   printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
+   return 0;
+}
